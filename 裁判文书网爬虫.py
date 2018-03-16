0429d047ea3bb98d3d73f14ced7876e55d63a87c
darthg <37092528+darthg@users.noreply.github.com>
2018-03-16 11:05:25 +0800

    较为能稳定运行的版本V1，能够提取授信客户清单的URL的标题
    待下一步继续挖掘

--- 裁判文书网爬虫.py
+++ 裁判文书网爬虫.py
@@ -4,34 +4,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import guoxiaoyi.xiciProxies
 from lxml import html
+import csv
 import time
-import  random
 import xlsxwriter
 import xlrd
+import urllib.parse
 #函数库
-
+def is_currentPage(xpathdata,i):
+     if xpathdata.xpath('//*[@id="pageNumber"]/span[2]/text()')[0]==i+1:
+          return  True
+     else:
+          return False
 
 def nextpage(browser):#查找下一页
-     nextpage=WebDriverWait(browser,300).until(
-          EC.presence_of_element_located((By.CLASS_NAME,"download"))
+     WebDriverWait(browser,5).until(
+          EC.presence_of_element_located((By.CLASS_NAME,"wstitle"))
      )#设置等待时间
-     time.sleep(1)
-     nextpage=browser.find_element_by_class_name("next")
-     while True:
-          if nextpage.is_enabled():
-               nextpage.click()
-               break
-          else:
-               pass
+     time.sleep(2)
+     elem=browser.find_element_by_class_name("next")
+     elem.click()
      return browser
 
 def myfind(x,y):
     return [ a for a in range(len(y)) if y[a] == x]
 
 def gettitles(titles):
-     for j in range(0,len(titles)):
-          i=myfind("|",titles[j])
-          titles[j]=titles[j][i[0]+1:i[1]]
+          i=myfind("|",titles)
+          return titles[i[0]+1:i[1]]
 
 def writeDataXls(urllist,titlelist,fp):
      workbook = xlsxwriter.Workbook(fp)
@@ -44,84 +43,107 @@ def writeDataXls(urllist,titlelist,fp):
                sheet.write(j+1,1,titlelist[i][j])
      workbook.close()
 
+try:
+#workbook=xlrd.open_workbook(r"E:\GitHub\测试文件\2016年保险明细报表.xls")
+     workbook=xlrd.open_workbook(r"E:\GitHub\测试文件\对公和小企业授信客户名称清单.xls")
+except Exception as e:
+     print("读取文件出现异常！请确认文件路径！")
+sheets_input=workbook.sheet_by_index(0)
+rows=sheets_input.nrows
 
 #----------------------------------------------------------------------------------
-#初始化各项参数
-url="http://wenshu.court.gov.cn/"
-keyword="索普"
+dicInput={'URL': '', '文书标题': ''}
+with open("E:\GitHub\测试文件\失信人测试结果\授信客户裁判文书网.csv",'a+',newline='') as csvfile:
+     fieldnames=['URL','文书标题']
+     writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
+     writer.writeheader()
 
-#-------------------------------------
-pat='href="(/content/content\?DocID=.*?&amp;KeyWord='+keyword+')"'##这里非常奇葩，有时候amp这个参数你源码中看不到，但是不要忘记加
-print(pat)
+#-----------------
+#初始化各项参数
+url="http://wenshu.court.gov.cn/list/list/?sorttype=1&number=2LDUFQ5Y&guid=665d8a6a-7472-8349765b-8eb1da9cfb43&conditions=searchWord+QWJS+++%E5%85%A8%E6%96%87%E6%A3%80%E7%B4%A2:"
 IPfp=r'E:\GitHub\测试文件\IP_Pool.xls'
 #------保存数据的变量
-urllist=[]
-titlelist=[]
-#获取代理IP
-#list=guoxiaoyi.xiciProxies.start_crawlProxies()
-#guoxiaoyi.xiciProxies.writeIPaddr(list,IPfp)
-Proxy=guoxiaoyi.xiciProxies.getPoxiesRand(IPfp)
 
-#设置请求头和获取代理池，设置代理IP
-browser=webdriver.Chrome#设置浏览器类型
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--proxy-server=http://'+Proxy)
-chrome_options.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
-browser = webdriver.Chrome(chrome_options=chrome_options)
+#-------------------------------------
 
 
-browser.get(url)
-elem1=browser.find_element_by_id("gover_search_key")
-elem1.clear()
-elem1.send_keys(keyword)
-searchbutton=browser.find_element_by_class_name("head_search_btn")#中国裁判文书网找这个搜索按钮一定要用
-searchbutton.click()
-time.sleep(3)
-elem1=WebDriverWait(browser,300).until(
-     EC.presence_of_element_located((By.CLASS_NAME,"dataItem"))
-)#设置等待时间
-data=browser.page_source#page_source方法输出的字符串，一定要Encode编码转换成二进制才能open出来
-#保存爬取的所需数据
-xpathdata=html.etree.HTML(data)
-title_count=xpathdata.xpath('//*[@id="span_datacount"]/text()')
-pageno=int(title_count[0])//5
-print("共有"+str(pageno)+"页")
 
-workbook = xlsxwriter.Workbook("E:/GitHub/测试文件/裁判文书网.xls")
-sheet=workbook.add_worksheet(keyword)
-sheet.write(0,0,'URL')
-sheet.write(0,1,'文书标题')
-sheet.write(0,2,'裁判时间')
-
-for i in  range(0,25):
+for k in range(212,rows):
      try:
-          WebDriverWait(browser,300).until(
-          EC.presence_of_element_located((By.CLASS_NAME,"download"))
-     )#设置等待时间
-          data=browser.page_source
-          xpathdata=html.etree.HTML(data)
-          urllist.append(xpathdata.xpath('//*[@id="resultList"]/div/table/tbody/tr/td/div/a[2]/@href'))
-          titlelist.append(xpathdata.xpath('//*[@id="resultList"]/div/table/tbody/tr/td/div/input[@type="hidden"]/@value'))
-          if len(urllist[i])!=0:
-               for j in range(0,len(urllist[i])):
-                    sheet.write(i*5+1+j,0,"http://wenshu.court.gov.cn"+urllist[i][j])
-                    index=myfind("|",titlelist[i][j])
-                    sheet.write(i*5+1+j,1,titlelist[i][j][index[0]+1:index[1]])
-                    sheet.write(i*5+1+j,2,titlelist[i][j][index[1]+1:])
-                    print("正在下载第"+str(i)+"页数据")
-          elif len(urllist[i])==0:
-               print("未采集到"+str(i)+"页数据")
-          nextpage(browser)
+          Proxy=guoxiaoyi.xiciProxies.getPoxiesRand(IPfp)
+          print("已经载入代理"+str(Proxy))
+          #设置请求头和获取代理池，设置代理IP
+          browser=webdriver.Chrome#设置浏览器类型
+          chrome_options = webdriver.ChromeOptions()
+          #chrome_options.add_argument('--proxy-server=http://114.215.45.174:3128')
+          chrome_options.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
+          browser = webdriver.Chrome(chrome_options=chrome_options)
+          keyword=sheets_input.cell_value(k,0)
+          pat='href="(/content/content\?DocID=.*?&amp;KeyWord='+keyword+')"'##这里非常奇葩，有时候amp这个参数你源码中看不到，但是不要忘记加
+          browser.get(url+urllib.parse.quote(keyword))
+          print("正在尝试查询授信客户:"+keyword)
           '''
-          if i!=0 and i%20==0:
-               Proxy=guoxiaoyi.xiciProxies.getPoxiesRand(IPfp)
-               browser.create_options().add_argument('--proxy-server=http://'+IPfp)
-               browser.create_options().add_argument('user-agent="Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36"')
-               time.sleep(3)'''
-     except Exception as e:
-          print(e)
-          workbook.close()
-          break
-workbook.close()
-browser.close()
+          elem1=browser.find_element_by_id("gover_search_key")
+          elem1.clear()
+          elem1.send_keys(keyword)
 
+
+          searchbutton=browser.find_element_by_class_name("head_search_btn")#中国裁判文书网找这个搜索按钮一定要用
+          searchbutton.click()
+          print("已提交查询信息，准备转向...")'''
+          time.sleep(3)
+          WebDriverWait(browser,300).until(
+               EC.presence_of_element_located((By.ID,"resultList"))
+          )#设置等待时间
+          WebDriverWait(browser,300).until(
+               EC.presence_of_element_located((By.CLASS_NAME,"wstitle"))
+          )#设置等待时间
+          data=browser.page_source#page_source方法输出的字符串，一定要Encode编码转换成二进制才能open出来
+          #保存爬取的所需数据
+          xpathdata=html.etree.HTML(data)
+          title_count=xpathdata.xpath('//*[@id="span_datacount"]/text()')
+          print("共有"+str(title_count[0])+"条数据")
+          if title_count[0]==0:
+               browser.close()
+               break
+          else:
+               if int(title_count[0])%5==0:
+                    pageno=int(title_count[0])//5
+               else:pageno=int(title_count[0])//5+1
+               print("共有"+str(pageno)+"页")
+
+               for i in  range(0,pageno):
+                    if i>=25:
+                         break
+                    print("正在载入"+str(i+1)+"页")
+                    data=browser.page_source
+                    xpathdata=html.etree.HTML(data)
+                    urllist=xpathdata.xpath('//*[@id="resultList"]/div/table/tbody/tr/td/div/a[2]/@href')
+                    titlelist=xpathdata.xpath('//*[@id="resultList"]/div/table/tbody/tr/td/div/input[@type="hidden"]/@value')
+                    print("当前文案标题为"+str(titlelist))
+                    print("urllist长度为"+str(len(urllist)))
+                    with open("E:\GitHub\测试文件\失信人测试结果\授信客户裁判文书网.csv",'a+',newline='',errors='ignore') as csvfile:#encoding=这个参数一定不要忘记加哦，不然会报错
+                         fieldnames=['URL','文书标题']
+                         writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
+                         for j in range(0,len(urllist)):
+                              dicInput['URL']=str('http://wenshu.court.gov.cn')+urllist[j]
+                              dicInput['文书标题']=gettitles(titlelist[j])
+                              writer.writerow(dicInput)
+                              print("已经写入第"+str(i*5+j+1)+"条数据")
+                    del urllist#尝试清空保存数据的变量
+                    del titlelist
+
+                    if i<pageno-1 and pageno!=1:
+                         browser=nextpage(browser)
+                         WebDriverWait(browser,300).until(
+                              EC.presence_of_element_located((By.CLASS_NAME,"wstitle"))
+                         )#设置等待时间
+                         time.sleep(2)
+                    else:
+                         break
+
+               browser.close()
+     except Exception as e:
+          print(keyword+"信息采集失败！")
+          browser.close()
+          continue
